9955004f43d7dabf377916ce8a6b012f94525e48
ivaylo-cl <57893475+ivaylo-cl@users.noreply.github.com>
2020-02-18 16:52:47 +0200

    Update create_avd.py

--- create_avd.py
+++ create_avd.py
@@ -1,37 +1,29 @@
 import subprocess
 
-android_systems = [
-    'android-22',
-    'android-23',
-    'android-24',
-    'android-25',
-    'android-27',
-    'android-28',
-    'android-29'
-]
-
-device_names = [
-    'pixel_5.1',
-    'pixel_6.0',
-    'pixel_7.0',
-    'pixel_7.1',
-    'pixel_8.1',
-    'pixel_9.0',
-    'pixel_10.0'
-]
+android_systems = {
+    'android-22': 'pixel_5.1',
+    'android-23': 'pixel_6.0',
+    'android-24': 'pixel_7.0',
+    'android-25': 'pixel_7.1',
+    'android-27': 'pixel_8.1',
+    'android-28': 'pixel_9.0',
+    'android-29': 'pixel_10.0',
+}
 
 def install_system():
-    for system in android_systems:
+    for system in android_systems.keys():
         print('Installing %s' % system)
         command = 'sdkmanager --install "system-images;%s;google_apis;x86_64'
         subprocess.check_output(command % system, shell=True)
 
 def create_avd():
-    for avd_names in device_names:
-        print('Creating %s' % avd_names)
+    for android_version, android_device in android_systems.items():
+        print(android_version, android_device)
+        print('Creating %s' % android_device)
         subprocess.check_output('echo "no" | avdmanager --verbose create avd --force --name "{0}" '
                                 '--device "pixel"--package "system-images;{1};google_apis;x86_64"'
-                                ' --tag "google_apis" --abi "x86_64"'.format(avd_names, android_systems), shell=True)
+                                ' --tag "google_apis" --abi "x86_64"'.format(android_device, android_version),
+                                shell=True)
         print('Done')
 
 install_system()

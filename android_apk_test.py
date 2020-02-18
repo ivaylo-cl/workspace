6708c729ed3615d60037016c504d3a15042ec7fc
ivaylo-cl <57893475+ivaylo-cl@users.noreply.github.com>
2020-02-18 13:58:35 +0200

    Update android_apk_test.py

--- android_apk_test.py
+++ android_apk_test.py
@@ -2,6 +2,22 @@ import os
 import subprocess
 import time
 import psutil
+import argparse
+
+emulator_names = [
+    'pixel_10.0',
+    'pixel_9.0',
+    'pixel_8.1',
+    'pixel_7.1',
+    'pixel_7.0',
+    'pixel_6.0',
+    'pixel_5.1',
+]
+
+gles_to_test = [
+    'gles2',
+    'gles3',
+]
 
 def kill_process():
     emulator = 'qemu-system-x86_64.exe'
@@ -24,6 +40,7 @@ def wait_for_output(open_emulator, expected_output, seconds_to_wait):
 
 
 def start_emulator(emulator_name):
+    ''' Starts an emulator and returns a reference to it, given its name'''
     print('Starting emulator')
     command = 'emulator -avd %s -wipe-data'
     return subprocess.Popen(command % emulator_name, stdout=subprocess.PIPE,
@@ -31,10 +48,11 @@ def start_emulator(emulator_name):
 
 
 def test_android_apk():
-    emulator_path = os.path.abspath('C:\\Users\\ivaylo.nichaev\\AppData\\Local\\Android\\Sdk\\emulator\\bin64')
+    emulator_path = os.path.abspath(os.getenv('LOCALAPPDATA') + '\\Android\\Sdk\\emulator\\bin64')
+
     directory_path = os.path.dirname(emulator_path)
     os.chdir(directory_path)
-    apk_path = os.path.abspath('C:\\Gameface-0.0.0.0-builds\\Gameface-0.0.0.0-Pro-Samples-Android\\Android')
+    apk_path = os.path.abspath('Samples/build')
     files = []
     # r=root, d=directories, f=files
     for r, d, f in os.walk(apk_path):
@@ -43,67 +61,37 @@ def test_android_apk():
                 files.append(os.path.join(r, file))
 
     for f in files:
-        boot_device = b'emulator: INFO: boot completed\r\n'
-        try:
-            if 'GLES2.apk' in f:
-                open_emulator = start_emulator('Pixel_XL_9.0')
+        for emulator_name in emulator_names:
+            boot_device = b'emulator: INFO: boot completed\r\n'
+            try:
+                if '.apk' in f:
+                    open_emulator = start_emulator(emulator_name)  # Generic_9.0 / Pixel_XL_9.0
 
-                wait_for_output(open_emulator, boot_device, 300)
-                print('Installing: ', f)
+                    wait_for_output(open_emulator, boot_device, 300)
+                    print('Installing: ', f)
 
-                subprocess.check_output("adb install " + f, shell=True)
-                print('Install success')
-                time.sleep(3)
+                    subprocess.check_output('adb install ' + f, shell=True)
+                    print('Install success')
+                    time.sleep(3)
 
-                subprocess.check_output('adb shell monkey -p com.coherentlabs.coherentsample.gles2 -v 500',
-                                 shell=True)
-                time.sleep(2)
+                    for gles in gles_to_test:
+                        subprocess.check_output('adb shell monkey -p com.coherentlabs.coherentsample.%s -v 500' % gles,
+                                         shell=True)
+                        time.sleep(2)
 
-                subprocess.Popen('adb shell pm clear com.coherentlabs.coherentsample.gles2', shell=True)
-                print('Close apk')
-                time.sleep(2)
+                        subprocess.Popen('adb shell pm clear com.coherentlabs.coherentsample.%s' % gles, shell=True)
+                        print('Close apk')
+                        time.sleep(2)
 
-                subprocess.check_output('adb uninstall com.coherentlabs.coherentsample.gles2', shell=True)
-                print('Uninstall success')
-                time.sleep(3)
+                        subprocess.check_output('adb uninstall com.coherentlabs.coherentsample.%s' % gles, shell=True)
+                        print('Uninstall success')
+                        time.sleep(3)
+                        kill_process()
+                        time.sleep(2)
+
+            except Exception as e:
+                print("Error while installing %s: %s" % (f, str(e)))
                 kill_process()
-                time.sleep(2)
-
-                # subprocess.Popen('emulator -avd Pixel_XL_9.0 -wipe-data', shell=True)
-                # time.sleep(2)
-                # kill_process()
-                # time.sleep(1)
-
-            if 'GLES3.apk' in f:
-                open_emulator2 = start_emulator('Pixel_3_XL_9.0')
-
-                wait_for_output(open_emulator2, boot_device, 300)
-                print('Installing: ', f)
-
-                subprocess.check_output('adb install ' + f)
-                print('Install success')
-                time.sleep(3)
-
-                subprocess.Popen('adb shell monkey -p com.coherentlabs.coherentsample.gles3 -v 500',
-                                 shell=True)
-                time.sleep(2)
-
-                subprocess.Popen('adb shell pm clear com.coherentlabs.coherentsample.gles3', shell=True)
-                print('Close apk')
-                time.sleep(2)
-
-                subprocess.Popen('adb uninstall com.coherentlabs.coherentsample.gles3', shell=True)
-                time.sleep(3)
-                kill_process()
-                time.sleep(2)
-
-                # subprocess.Popen('emulator -avd Pixel_3_XL_9.0 -wipe-data', shell=True)
-                # kill_process()
-                # time.sleep(3)
-
-        except Exception as e:
-            print("Error while installing %s: %s" % (f, str(e)))
-            kill_process()
-            time.sleep(1)
-            continue
+                time.sleep(1)
+                continue
 test_android_apk()
